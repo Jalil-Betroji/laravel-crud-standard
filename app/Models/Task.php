c9214f733bc4b44ce434e43b5a55ad20bea45f14
jalil betroji <jalil.betroji@gmail.com>
2024-01-11 16:32:30 +0100

    models and controllers added

---
+++ app/Models/Task.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'description',
+        'projectId',
+        'start_date',
+        'end_date',
+    ];
+    public function projects(){
+        return $this->belongsTo(Project::class);
+    }
+}
